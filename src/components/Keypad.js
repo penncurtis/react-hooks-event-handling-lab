a8bc681824be728e7c2e473fa3e7e6edbee5b544
Penn Curtis <penn.s.m.c@gmail.com>
2023-05-02 09:41:54 -0400

    complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
+import React from "react"
+
 function Keypad (){
+
+    function handleEvent(){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <input type="password" onChange={handleEvent} />
     )
 }
 
